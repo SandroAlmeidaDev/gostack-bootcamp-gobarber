e9903fbf9e4a4f49625bfe536ddfdc3fe16f6243
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-06-21 22:06:24 -0300

    Configurado injeção de dependencias

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,9 +1,10 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepositoy from '../repositories/IUsersRepository';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -17,8 +18,12 @@ interface IResponse {
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-  constructor(private usersRepository: IUsersRepositoy) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
-/* eslint-disable no-console */
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+// import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import 'express-async-errors';
 
@@ -9,13 +9,22 @@ import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
+
+// Sentry.init({
+//  dsn:
+//    'https://fe53c460874c4625b7694b73a3100290@o349623.ingest.sentry.io/3026376',
+// });
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+// app.use(Sentry.Handlers.requestHandler());
 app.use(routes);
+
+// app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
@@ -24,6 +33,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
+
+  // console.error(err);
 
   return response.status(500).json({
     status: 'error',
